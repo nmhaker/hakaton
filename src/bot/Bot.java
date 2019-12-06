4cfeaae2b48fd2cccccbb230d44b0cde2c8f6f25
mladjankg <mladjan.mihajlovickg@gmail.com>
2019-12-07 00:17:15 +0100

    Added bot class

---
+++ src/bot/Bot.java
@@ -0,0 +1,14 @@
+package bot;
+
+public class Bot {
+
+    public void run() {
+        //state = Join game
+        //while not winner = null
+        //decide move
+
+        //do action
+
+        //loop
+    }
+}
--- src/bot/Brain.java
+++ src/bot/Brain.java
@@ -1,4 +1,6 @@
 package bot;
 
 public class Brain {
+
+
 }
