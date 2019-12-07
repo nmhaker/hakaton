88626105b15e981cf18bf6991ecba2b364c12bc9
Stefan Brankovic <stefanbrankovic96@gmail.com>
2019-12-07 11:19:16 +0100

    Zapocet BuildState

--- src/states/BuildState.java
+++ src/states/BuildState.java
@@ -1,5 +1,7 @@
 package states;
 
+import bot.Helpers;
+
 public class BuildState extends State {
 
     private int remainingWood;
@@ -7,27 +9,43 @@ public class BuildState extends State {
     private int remainingMetal;
     private int nextBuilding;
     enum BuildSubStates {
-
+        GET_3_STONES,
+        GET_2_WOODS,
+        BUILD_FORTIFY,
+        GET_3_METAL,
+        GET_1_WOOD,
+        BUILD_FORTIFY_SWORD,
+        GET_2__WOODS,
+        GET_1_STONE,
+        BUILD_HOUSE,
+        TAKE_SWORD
     }
+
+    BuildSubStates buildSubState = BuildSubStates.GET_3_STONES;
 
     private static BuildState build_state = null;
     private BuildState() {}
 
     public static BuildState getInstance()
-    {
+    {.
         if (build_state == null)
             build_state = new BuildState();
 
         return build_state;
-    }
+    }..
     @Override
     public String chooseAction() {
-        // findClosestShop
-        // go to shop
-        // take resource
-        // need more? yes -> go to start
-        // go to building
-        // build
-        return null;
+        if(threat()){
+
+        }else{
+            switch(buildSubState){
+                case GET_3_STONES:{
+                    if(!Helpers.nextTo())
+                }
+                break;
+            }
+            return null;
+        }
+
     }
 }
